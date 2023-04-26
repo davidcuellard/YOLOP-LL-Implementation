a70d33b4346da0a5b70839a93df523c35339fb98
davidcuellard <david.cuellar@javeriana.edu.co>
2023-04-26 14:31:14 +0000

    fix

--- lib/config/default.py
+++ lib/config/default.py
@@ -50,10 +50,10 @@ _C.LOSS.LL_IOU_GAIN = 0.2 # lane line iou loss gain
 
 # DATASET related params
 _C.DATASET = CN(new_allowed=True)
-_C.DATASET.DATAROOT = '/notebooks/dataset-yolop100/images'       # the path of images folder
-_C.DATASET.LABELROOT = '/notebooks/dataset-yolop100/det_annotations'      # the path of det_annotations folder
-_C.DATASET.MASKROOT = '/notebooks/dataset-yolop100/bdd_seg_gt'                # the path of da_seg_annotations folder
-_C.DATASET.LANEROOT = '/notebooks/dataset-yolop100/bdd_lane_gt'               # the path of ll_seg_annotations folder
+_C.DATASET.DATAROOT = '/notebooks/YOLOP-LL-Implementation/dataset-yolop100/images'       # the path of images folder
+_C.DATASET.LABELROOT = '/notebooks/YOLOP-LL-Implementation/dataset-yolop100/det_annotations'      # the path of det_annotations folder
+_C.DATASET.MASKROOT = '/notebooks/YOLOP-LL-Implementation/dataset-yolop100/bdd_seg_gt'                # the path of da_seg_annotations folder
+_C.DATASET.LANEROOT = '/notebooks/YOLOP-LL-Implementation/dataset-yolop100/bdd_lane_gt'               # the path of ll_seg_annotations folder
 _C.DATASET.DATASET = 'BddDataset'
 _C.DATASET.TRAIN_SET = 'train'
 _C.DATASET.TEST_SET = 'val'
